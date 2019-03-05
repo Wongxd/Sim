7a82337a8ec9236de09ba7acdcef0b225d29dfca
wongxd <wxd1@live.com>
2019-03-05 14:54:30 +0800

    观察者添加泛型约束

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Step 1. Add it in your root build.gradle at the end of repositories:
 Step 2. Add the dependency
 
 	dependencies {
-	        compile 'com.github.Wongxd:SimpleBus:0.0.1'
+	        compile 'com.github.Wongxd:SimpleBus:x.y.z'
 	}
 
 
--- libraykt/src/main/java/io/github/wongxd/simplebuskt/observer/IObserver.kt
+++ libraykt/src/main/java/io/github/wongxd/simplebuskt/observer/IObserver.kt
@@ -3,7 +3,7 @@ package io.github.wongxd.simplebuskt.observer
 /**
  * 事件订阅者（观察者）
  */
-interface IObserver {
+interface IObserver<T> {
 
     /**
      * 当消息发布者发布事件的时候调用该方法
@@ -11,6 +11,6 @@ interface IObserver {
      * @param event 发布的事件
      * @param code  事件标记（用于确定哪些接收者应该接收到事件）
      */
-    fun onMessageReceived(event: Any, code: Int)
+    fun onMessageReceived(event: T, code: Int)
 
 }
